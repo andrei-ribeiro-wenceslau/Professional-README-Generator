c9f106004b66ee23fb315c3aec29c2e29728a292
Andrei <andrei.ribeirow@gmail.com>
2023-07-21 19:20:46 +1000

    added inquirer and filesystem packages to application. Added prompt code using the inquirer package and array of questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,86 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "What is your GitHub username",
+    "What is your email address?",
+    "What is the project name?",
+    "Please write a short description of your project.",
+    "What kind of license should your project have?",
+    "What command should be run to install dependencies?",
+    "What command should be run to run tests?",
+    "What does the user need to know about using this repo?",
+    "What does the user need to know about contributing to the repo?"
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+   
+   inquirer
+  .prompt([
+      {
+          type: 'input',
+          name: 'githubUserName',
+          message: questions[0],
+      },
+      {
+          type: 'input',
+          name: 'email',
+          message: questions[1],
+      },
+      {
+          type: 'input',
+          name: 'projectName',
+          message: questions[2],
+      },
+      {
+          type: 'input',
+          name: 'description',
+          message: questions[3],
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: questions[4],
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', "BSD 3", "None"],
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: questions[5],
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: questions[6],
+    },
+    {
+        type: 'input',
+        name: 'information',
+        message: questions[7],
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: questions[8],
+    },
+  ])
+  .then((answers) => {
+//     const htmlPageContent = generateHTML(answers);
 
+//     fs.writeFile('index.html', htmlPageContent, (err) =>
+//       err ? console.log(err) : console.log('Successfully created index.html!')
+//     );
+//   });
+console.log(answers);
+})
+}
 // Function call to initialize app
 init();
+
+
